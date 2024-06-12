b3472a18b281045af08b918341214678c9e91455
Alokesh07 <alokeshbhatt2003@gmail.com>
2024-06-12 23:21:34 +0530

    Many files are updated

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// JavaScript functionality for interactive elements
+
+// Example function to be executed on dropdown item click
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+// Example function for booking
+function bookPackage() {
+    alert("Booking functionality to be implemented.");
+}
+
+// Add event listeners or any other JS code as needed
